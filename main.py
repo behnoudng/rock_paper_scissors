0a3e3e6e48af8e2eac464d29b028b655a0c4a35f
behnoudng <behnoudng@gmail.com>
2024-12-05 11:41:34 +0330

    Fixed the while loop errors

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
 import os
+
+
 def rock_paper_scissors():
     score1, score2 = 0, 0
     exit = False
-    while exit == False :
+    while exit == False:
         gamer1 = input("Enter your choice: ")
-        # os.system("clear")
+        os.system("cls")
 
-        if (gamer1 == "exit"):
-            print("gamer1 score : ", score1 )
-            print("gamer2 score : ", score2 )
+        if gamer1 == "exit":
+            print("gamer1 score : ", score1)
+            print("gamer2 score : ", score2)
             exit = True
+            break
         gamer2 = input("It's your turn: ")
 
-
-
         r, p, s = "rock", "paper", "scissors"
-        if gamer1 == r and gamer2 == p :
+        if gamer1 == r and gamer2 == p:
             print("gamer2 is the winner")
             score2 += 1
-        elif gamer1 == r and gamer2 == s :
+        elif gamer1 == r and gamer2 == s:
             print("gamer1 is the winner")
             score1 += 1
-        elif gamer1 == p and gamer2 == r :
+        elif gamer1 == p and gamer2 == r:
             print("gamer1 is the winner")
             score1 += 1
-        elif gamer1 == p and gamer2 == s :
+        elif gamer1 == p and gamer2 == s:
             print("gamer2 is the winner")
             score2 += 1
-        elif gamer1 == s and gamer2 == r :
+        elif gamer1 == s and gamer2 == r:
             print("gamer2 is the winner")
             score2 += 1
-        elif gamer1 == s and gamer2 == p :
+        elif gamer1 == s and gamer2 == p:
             print("gamer1 is the winner")
             score1 += 1
-        elif gamer1 == gamer2 :
+        elif gamer1 == gamer2:
             print("draw")
 
 
-
 rock_paper_scissors()
-
-
-
 
